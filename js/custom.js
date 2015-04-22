1ec23b2e3ea9a8315b295b13f3fcb70f0563ab16
nathangrotticelli <nathangrotticelli@gmail.com>
2015-04-22 18:19:00 -0400

    mail fix6

--- js/custom.js
+++ js/custom.js
@@ -214,7 +214,8 @@ $("#contact").submit(function (e) {
     var email = $("#email").val();
     // var subject = $("#subject").val();
     var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+
+    // var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
 
     function isValidEmail(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -224,7 +225,8 @@ $("#contact").submit(function (e) {
         $.ajax({
             type: "POST",
             url: "sendmail.php",
-            data: dataString,
+            dataType: 'json',
+            data: $('#contact').serialize(),
             success: function () {
                 $('.success').fadeIn(1000);
                 $('.error').fadeOut(500);
