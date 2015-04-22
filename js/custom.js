1b0b90f41f5ea1f794185103b766fbedfa0b7ac8
nathangrotticelli <nathangrotticelli@gmail.com>
2015-04-22 18:45:08 -0400

    mail fix10

--- js/custom.js
+++ js/custom.js
@@ -208,35 +208,32 @@ $("#subscribe").submit(function (e) {
 /* =================================
 ===  CONTACT FORM          ====
 =================================== */
-$('.submit_contact').on('click', function(e){
-    e.preventDefault();
-    // var name = $("#name").val();
-    // var email = $("#email").val();
-    // var subject = $("#subject").val();
-    // var message = $("#message").val();
+    var submitContact = $('.submit_contact');
 
-    // var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+    submitContact.on('click', function(e){
+        e.preventDefault();
 
-    // function isValidEmail(emailAddress) {
-    //     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    //     return pattern.test(emailAddress);
-    // };
+        var $this = $(this),
+                message = $('.msg2');
 
+            $.ajax({
+                type: "POST",
+                url: 'contact.php',
+                dataType: 'json',
+                cache: false,
+                data: $('.contact-work-form2').serialize(),
+                success: function(data) {
 
-        $.ajax({
-            type: "POST",
-            url: "sendmail.php",
-            dataType: 'json',
-            cache: false,
-            data: $('.sA1').serialize(),
-            success: function () {
-                $('.success').fadeIn(1000);
-                $('.error').fadeOut(500);
-            }
-        });
+                    if(data.info !== 'error'){
+                        $this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
+                        message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(2350).fadeOut('slow');
+                    } else {
+                        message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(2000).fadeOut('slow');
+                    }
+                }
+            });
 
-    return false;
-});
+    });
 
    // $("#contact").submit(function (e) {
    //      e.preventDefault();
